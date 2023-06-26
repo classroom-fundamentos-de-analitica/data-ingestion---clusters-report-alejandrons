798190989e164d4e46d1f5dd38fd9b14f59278e2
alejandrons <80337888+alejandrons@users.noreply.github.com>
2023-06-26 04:28:37 -0500

    Attempt 1

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,35 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    
+    with open("clusters_report.txt",'r') as file:
+        raw_file = file.readlines()
+    data = list()
+    head = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
+    raw_file = raw_file[4:]
+    
+    for line in raw_file:
+        words = line.split()
+        if len(words) != 0:
+            if words[0].isdigit():
+                data.append(words[0:3])
+                data[-1].append(' '.join(words[4:]))
+                data[-1][-1] += ' '
+            else:
+                data[-1][-1] += ' '.join(words)
+                data[-1][-1] += ' '
+        
+    for line in data:
+        line[0] = int(line[0])
+        line[1] = int(line[1])
+        line[2] = line[2].replace(',','.')
+        line[2] = float(line[2])
+        line[-1] = line[-1].rstrip('. ')
+        line[-1] = line[-1].replace('-',' ')
+    
+    df = pd.DataFrame(data,columns=head)
 
     return df
+
+
+print(ingest_data())
